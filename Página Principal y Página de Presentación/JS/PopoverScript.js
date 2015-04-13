11be42f7b16b967bfdf90593873ecb13a174a0f1
Jasson Moya <jasc996@gmail.com>
2015-04-13 11:10:47 -0600

    Página Principal y Página de Presentación...

---
+++ Página Principal y Página de Presentación/JS/PopoverScript.js
@@ -0,0 +1,6 @@
+    $(".configPop").popover({
+      placement: "bottom",
+      title: "Configuración",
+      content: "<a class='BotonPop' href='#'>Editar perfil</a><li class='divider'></li><a class='BotonPop' href='#'>Cerrar sesión</a>",
+      html: true
+    })
