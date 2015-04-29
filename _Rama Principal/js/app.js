385413de8f57a5fef83af9d709e0677b1a821501
jm95aguilar <jm95aguilar@hotmail.com>
2015-04-29 14:33:43 -0600

    Modificacion Notificaciones

--- _Rama Principal/js/app.js
+++ _Rama Principal/js/app.js
@@ -10,7 +10,7 @@ app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/users', {templateUrl: 'partials/users.html', controller: 'usersCtrl', css: 'css/styleUsers.css'});
   $routeProvider.when('/pet', {templateUrl: 'partials/pet.html', controller: 'petCtrl', css: 'css/stylePet.css'});
   $routeProvider.when('/perfilMascotas', {templateUrl: 'partials/perfilMascotas.html', controller: 'perfilMascotasCtrl', css: 'css/petProfileStyle.css'});
-  $routeProvider.when('/notificaciones', {templateUrl: 'partials/notificaciones.html', controller: 'notificacionesCtrl', css: 'css/widgets.min.css'});
+  $routeProvider.when('/notificaciones', {templateUrl: 'partials/notificaciones.html', controller: 'notificacionesCtrl', css: ['css/widgets.min.css','css/notificaciones.css']});
   
   $routeProvider.otherwise({redirectTo: '/inicio'});
 }]);
