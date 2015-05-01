0dcfa33994058b17b976f1e7970099bdc1093bf8
jm95aguilar <jm95aguilar@hotmail.com>
2015-04-30 21:51:19 -0600

    Update Lista Negra

--- _Rama Principal/js/controllers/listanegraCtrl.js
+++ _Rama Principal/js/controllers/listanegraCtrl.js
@@ -1,12 +1,13 @@
 'use strict'
 
-app.controller('listanegraCtrl',function($scope,$location){
+app.controller('listanegraCtrl',function($scope,$location,$http){
 	$scope.listanegra = function(){
 		$location.path('/listanegra');
 	};
 
 	$scope.cargarlistanegra = function(){
-		$http.get('conectUserTableLN.php').success(function(data) { $scope.posts = data; });
+		console.log('Esto no sirve');
+		$http.get('data/conectUserTableLN.php').success(function(data) { $scope.posts = data; });
 		
 	};
 	$scope.atras = function(){
