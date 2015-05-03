7bd6855b9a8dfd06d22f373208e2af73c9265e75
jm95aguilar <jm95aguilar@hotmail.com>
2015-05-03 17:45:35 -0600

    Cambio Notificaciones
    Cambio de forma a un solo botón, guarda la variable del ID FORMULARIO en
    el sessionStorage para ser usada por verRespuestas

--- _Rama Principal/js/controllers/notificacionesCtrl.js
+++ _Rama Principal/js/controllers/notificacionesCtrl.js
@@ -1,10 +1,15 @@
 'use strict'
-
+ 
 app.controller('notificacionesCtrl', function($scope,$location,$http){
 	$scope.inicializarNot = function(){
 		$scope.varUser = JSON.parse(sessionStorage.user);
-	$http.post('data/conectNotifyTable.php',$scope.varUser)
-	.success(function(data) { 
+		$http.post('data/conectNotifyTable.php',$scope.varUser)
+		.success(function(data) { 
 		$scope.posts = data; })
+	}
+	$scope.guardarID = function(pID){
+		sessionStorage.tempIDForm = pID;
+		console.log(sessionStorage.getItem('tempIDForm'));
+		$location.path('/resultados');
 	};
 });
